351ecc23c518f4c86c7123c5e322db88de9f77ad
wujunwen <junwen.wu@qunar.com>
2014-07-21 12:13:26 +0800

    first blood

---
+++ Alliance/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Alliance
+//
+//  Created by junwen.wu on 14-7-20.
+//  Copyright (c) 2014年 Bwl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Alliance/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Alliance
+//
+//  Created by junwen.wu on 14-7-20.
+//  Copyright (c) 2014年 Bwl. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
